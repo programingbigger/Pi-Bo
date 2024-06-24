3c8f7affc48ffaf934a191f515e071628dc676d0
programingbigger <shidiannaigong@gmail.com>
2024-06-24 15:12:09 +0100

    並列処理を施しました。その第一弾。もう少し早い動作に切り替えたりしたい

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 - todo
   - main.py
 	- faces.pyとcapture.pyを同時に処理させる並列処理の実装
+	- 一応、ThreadPoolExcuterで実装できたけど、いかがなものか・・・
 	- add clean.py
 
   - MY_LOGGING.py
@@ -18,11 +19,16 @@
 		- classを定義し、見やすいような形にする
   - faces.py
 	- tryとexceptをなくした状態にし、main.pyに移行させる
+	- 顔をランダムに出力するアーキテクチャに書き換える
+
 """
 from components import clean
 from components import faces
 from components import capture
 from LOGGING.MY_LOGGING import setup_log
+from concurrent.futures import ThreadPoolExecutor
+import signal
+import sys
 
 # setup loggig
 main_logger = setup_log(__name__)
@@ -44,12 +50,47 @@ main_logger.info("start main.py")
 	# ~ faces.smile()
 	# ~ faces.trouble()
 
-# capture
-while True:
+# 電光掲示板の実行関数
+def emojishow():
+	# ~ faces.magao()
+	# ~ faces.eyes_LR()
+	faces.smile()
+	# ~ faces.trouble()
+
+# カメラの実行関数
+def face2marquee():
+	capture.CaptureFace()
+
+# 
+def signal_handler(signum, frame):
+  print("stop")
+  executor.shutdown(wait=False)
+  sys.exit(0)
+  
+# set signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+# 並列処理の実装
+with ThreadPoolExecutor(max_workers=2) as executor:
 	try:
-		capture.CaptureFace()
+		while True:
+			emoji = executor.submit(emojishow)
+			face = executor.submit(face2marquee)
+		 
+			emoji = emoji.result()
+			face = face.result()
+		 
+			print("do ThreadPoolExecutor")
 	except KeyboardInterrupt:
-		# Ctrl+C でループ終了
-		capture.StopCamera()
+		pass
+
+
+# ~ # capture
+# ~ while True:
+	# ~ try:
+		# ~ capture.CaptureFace()
+	# ~ except KeyboardInterrupt:
+		# ~ # Ctrl+C でループ終了
+		# ~ capture.StopCamera()
 
 main_logger.info("end main.py")
